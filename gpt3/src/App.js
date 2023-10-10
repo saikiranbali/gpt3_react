04c3e23ae962931b2695c87c4cac204693ca91ba
saikiranbali <balisaikiran@gmail.com>
2023-10-10 11:48:02 +0300

    added css

--- gpt3/src/App.js
+++ gpt3/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import './App.css'
 import { Article,Brand,CTA,Feature,Navbar } from './components'
 import { Footer, Blog, Possibility, Features, WhatGPT3, Header } from './containers'
 const App = () => {
