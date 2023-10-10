a9eddbbe55d2506ef6840a8dfef0df2ed546e5d0
saikiranbali <balisaikiran@gmail.com>
2023-10-10 14:41:31 +0300

    navbar

--- gpt3/src/components/navbar/navbar.jsx
+++ gpt3/src/components/navbar/navbar.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import './navbar.css'
+import {RiMenu3Line , RiCloseLine} from 'react-icons/ri'
+import logo from '../../assets/logo.svg'
+
+
+
+// BEM - Block Element Modifier. we use this to name our css class conventions
+
 const navbar = () => {
   return (
-    <div>
-      Navbar
+    <div className='gpt__navbar'>
+      <div className='gpt__navbar-links'>
+        <div className='gpt__navbar-links_logo'>
+          <img src={logo} alt="logo" />
+        </div>
+      </div>
     </div>
   )
 }
